d8a8db0103042f7827ec89d6221c2da7446bd5ac
Steven Tingiris <steve@dabblelab.com>
2020-06-20 14:18:04 -0400

    update eslintrc

--- lambda/.eslintrc.js
+++ lambda/.eslintrc.js
@@ -11,5 +11,10 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
+    'no-console': 'off',
+    'no-use-before-define': ['error', { functions: false }],
+    'prefer-destructuring': 'off',
+    'quote-props': ['error', 'consistent'],
+    'object-shorthand': 0,
   },
 };
